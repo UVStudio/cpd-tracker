7288ee2d2aff518a99bb512a2ccdb11eb13f2365
Leonard Shen <uvstudio2199@gmail.com>
2022-07-20 18:49:35 -0400

    email fn styled

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -285,12 +285,26 @@ exports.sendMassEmails = asyncHandler(async (req, res, next) => {
           /* required */
           Html: {
             Charset: 'UTF-8',
-            Data: `Hello ${
-              currentUser.name
-            }, for testing purposes, your email address is ${currentUser.email}.
-            You currently need ${
+            Data: `<h3>Hello ${currentUser.name}:</h3>
+            <p>For testing purposes, your email address is ${
+              currentUser.email
+            }.</p>
+            <p>You currently need <b>${
               totalRollingCPDHoursRequired - CPDAccomplishedThreeYears
-            } hours for this year of ${currentYear}.
+            }</b> total CPD hours for the year ${currentYear} to fulfill all your CPD requirements for the province of ${
+              currentUser.province
+            }. Of which, <b>${
+              currentYearNeedVerHours - currentUser.hours[0].verifiable
+            }</b> hour(s) will need to be verifiable.</p>
+            <p>If you do not wish to receive similar emails from us in the future, please reply to this email requesting to be removed from our email notification list. A human will manually remove your email address from this list.</p>
+
+            <p>Thank you for using CPD Tracker by Sheriff Consulting.</p>
+            <p><b>
+            Leonard Shen, Application Developer<br>
+            Sheriff Consulting</b><br>
+            <br>
+            </p>
+
             `,
           },
           Text: {
